90b5e84b2ca44d171151c7430f4cdf8fdca26937
MD Waliullah <waliullah.godev@gmail.com>
2026-01-13 01:52:37 +0600

    feat: separate layout for UI and dashboard

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/src/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <Button>Click me</Button>
+      <Button>Click me</Button>
     </div>
   );
 }
---
+++ src/app/(dashboardlayout)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/src/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Button>This is form dashvaord layout</Button>
+    </div>
+  );
+}
